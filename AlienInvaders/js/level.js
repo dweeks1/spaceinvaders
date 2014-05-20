20a49cccd04bd45eafa01b5f70620d669bc75b24
Danielle Weeks <i7959050@bournemouth.ac.uk>
2014-05-20 16:15:29 +0100

    add engine

---
+++ AlienInvaders/js/level.js
@@ -0,0 +1,98 @@
+//This is where the levels can be modified, added or changed and positioning of sprites on the screen can be changed. To add new levels, copy the previous level, change number and be sure to close bracket at end of all levels. 
+  var levelData = { 
+     1:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [1,1,1,1,1,1,1,1,1,1,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0]],
+//Number 1 is Nicki in frame 1, Robbie in frame 2, for Alien 2 sy needs to be moved down for Sprites located underneath.
+     2:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,1,1,1,1,1,1,1,1,1,0],
+          [0,1,1,1,1,1,1,1,1,1,0],
+          [0,1,1,1,1,1,1,1,1,1,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0]],
+      3:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0]],
+      4:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0]] };
+
+//This is where to change the aliens, player and lazers in terms of their positioning within the sprites.
+var spriteData = {
+    'alien1': { sx: 0,  sy: 0,  w: 41, h: 37, cls: Alien, frames: 2 },
+    'alien2': { sx: 0,  sy: 18, w: 23, h: 18, cls: Alien, frames: 2 },
+    'player': { sx: 103,  sy: 46, w: 62, h: 55, cls: Player },
+    'missile': { sx: 169,  sy:66, w: 31,  h: 34, cls: Missile }
+  }
+
+//This is the start up setup, opening new game board with instructions on how to play. 
+function startGame() {
+    var screen = new GameScreen("Music Invaders","press space to start",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+    Game.loop();
+  }
+
+//This is the end screen if the player gets defeated, with instructions to start afresh. This will then load new Gameboard screen 
+function endGame() {
+    var screen = new GameScreen("Game Over","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+
+//This function is setup for if the player defeats all the aliens in the levels. This then again gives the options to restart the game from afresh.   
+function winGame() {
+    var screen = new GameScreen("You Win!","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+//This function here is setup for the sounds that go with the game, and which function they are paired with, i.e fire sound and die sound. This also initializes the functions behind each movement within the game, so when gameboard is setup, level data and sprite data is setup, then if player dies, it is end of the game so shows end game function, or win shows win game function. 
+$(function() {
+    GameAudio.load({ 'fire' : 'media/laser.ogg', 'die' : 'media/explosion.ogg' }, 
+                   function() { 
+                       Game.initialize("#gameboard", levelData, spriteData,
+                                      { "start": startGame,
+                                        "die"  : endGame,
+                                        "win"  : winGame });
+                   });
+   });
+
+
+
